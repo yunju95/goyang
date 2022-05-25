fb94977d59e95d0b2571aeccbb6e8822a0c22352
yunju95 <yunju95@github.com>
2022-05-25 23:48:46 +0900

    [update] : test

--- js/common.js
+++ js/common.js
@@ -1,6 +1,12 @@
 
 $(function(){
-  $('.doc-gnb .list_menu').mouseenter(function(){
-    console.log('ddd');
-  });
+  $('.doc-gnb .list_menu').on( "mouseenter", function() {
+      $( this ).find('.list_menudep2').show(300);
+      $( this ).parent().find('.bg_depth2').show(300);
+    });
+
+      // $( this ).find('.list_menudep2').hide();
+      // $( this ).parent().find('.bg_depth2').hide();
+
+
 });
