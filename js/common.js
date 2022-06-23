3111a7544b3fb4552be1943c86121e91602651f2
yunju95 <yunju95@github.com>
2022-06-23 23:21:38 +0900

    [update] : 메인 슬라이드 수정건

--- js/common.js
+++ js/common.js
@@ -1,44 +1,27 @@
 $(function(){
-
+  /* 2022-06-23 수정 시작 */
   // main visual slider
   const autoplay = 5000;
   const swiperMv = new Swiper('.slide_mv', {
       effect: 'fade',
       navigation: {
         nextEl: ".slide_mv .swiper-button-next",
-        prevEl: ".slide_mv .swiper-button-prev"
+        prevEl: ".slide_mv .swiper-button-prev",
+        clickable: true,
       },
       autoplay: {
         delay: 5000,
+        disableOnInteraction: false,
       },
         pagination: {
           el: ".page_active",
           clickable: false,
           renderBullet: function (index, className) {
-              return '<span class="' + className + '">' + '0' + (index + 1) + "</span>";
+              return '<span class="' + className + '">' + '0' + (index + 1) + "<span class='outer_progress'><em class='progress'></em></span></span>";
           },
       },
-      onProgress: move(),
-      on: {
-        slideChange : function(){
-          move();
-        }
-      }
   });
-  function move() {
-      let elem = document.getElementById("progress"); 
-      let width = 1;
-      let autoplayTime = autoplay / 100;
-      let id = setInterval(frame, autoplayTime);
-      function frame() {
-          if (width >= 100) {
-              clearInterval(id);
-          } else {
-              width++; 
-              elem.style.width = width + '%'; 
-          }
-      }
-  };
+  /* 2022-06-23 수정 끝 */
   
   // main tab
   $('.wrap_tabenrolment .link_tab').click(function(e){
